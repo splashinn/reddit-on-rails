22d721048dc19d67de2d7cf66371ae0ceb518199
Kyle Maune <kmaune71@gmail.com>
2014-09-27 18:10:37 -0700

    restart project

--- railsit/db/migrate/20140928003331_create_memberships.rb
+++
@@ -1,9 +0,0 @@
-class CreateMemberships < ActiveRecord::Migration
-  def change
-    create_table :memberships do |t|
-      t.integer :user_ud
-      t.integer :group_id
-      t.timestamps
-    end
-  end
-end
